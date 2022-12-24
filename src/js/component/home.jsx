d90a9448c10aede2e5c455d99ac13ec61cb6fe06
BrodyDubach <brodydubach@gmail.com>
2022-12-24 01:19:46 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,88 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
+import Toggle from "./Toggle";
 
 //create your first component
-const Home = () => {
+//Parent Component
+function Home(){
+	const [task, setTask] = useState("");
+	const [variable, setVariable] = useState([
+		"Do Homework",
+		"Do Laundry",
+		"Walk the Dog"
+	]);
+	// console.log(variable)
+
+	let todo = variable.map((item, i) => {
+		return (
+			<li className="list-group-item item" key={i}>
+				{item}
+				<div
+					className="mouseOver"
+					style={{ float: "right" }}
+					onClick={() => removeItem(i)}>
+					X
+				</div>
+			</li>
+		);
+	});
+
+	//Add Function
+	const newTodo = onKeyDownEvent => {
+		// console.log(onKeyDownEvent);
+		if (onKeyDownEvent.key === "Enter") {
+			const newTodo = [...variable, task];
+			setVariable(newTodo);
+			setTask("");
+		}
+	};
+
+	//Delete Function
+	const removeItem = index => {
+		// console.log(index);
+		const newArray = variable.filter((item, i) => i !== index);
+		setVariable(newArray);
+	};
+
+	
+	//Edit Function
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="">TODO's</h1>
+				<input 
+					className="" 
+					placeholder="Todo's"
+					value={task}
+					onChange={e => setTask(e.target.value)}
+					type="text"
+					id="fname"
+					name="fname"
+					onKeyDown={newTodo}
+				/>
+			<div>
+				<ul>{todo}</ul>
+				<div className="list-group-item footer">
+					{todo.length} item left
+				</div>
+			</div>
+			<p><Toggle /></p>
 		</div>
 	);
 };
 
 export default Home;
+
+
+// const styles = {
+// 	container: {
+// 		display: 'flex',
+// 		flexDirection: 'row',
+// 		background: 'black'
+// 	},
+// 	h1: {
+// 		fontSize: '10px'
+// 	}
+// };
+
